1937c6020faa0dc615989833ba298ca7d6ad8d9c
AlexKasema <lexxmulwa@gmail.com>
2024-01-03 00:36:50 +0300

    Display random memes using state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,29 @@
 
 import React from "react"
 
+import memesData from "../memesData"
+
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        
+        const randomIndex = 
+            Math.floor(Math.random() * memesArray.length);
+        const randomUrl = memesArray[randomIndex].url;
+        
+        setMemeImage(randomUrl)
+    }
     return (
         <main>
-            <form className="meme--form">
+            <div className="meme--form">
                 <input type="text" className="form--input" placeholder="Top Text" />
                 <input type="text" className="form--input" placeholder="Bottom Text" />
-                <button className="form--button">Get a new meme image  🖼</button>
-            </form>
+                <button onClick={getMemeImage} className="form--button">Get a new meme image  🖼</button>
+            </div>
+            <img className="meme--image" src={memeImage} alt="meme" />
         </main>
     )
 }
